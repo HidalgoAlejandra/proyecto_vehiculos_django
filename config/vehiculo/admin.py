2484faa653d87a2223d6eb241bfa1705467606d5
Alejandra <informaticotrabajo00@gmail.com>
2023-07-16 22:18:18 -0400

    admin

--- config/vehiculo/admin.py
+++ config/vehiculo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Vehiculo
+
+class VehiculoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created','updated')
+
+admin.site.register(Vehiculo,VehiculoAdmin)
